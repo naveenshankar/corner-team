06ef9897710e299270ecac3f1661211c3d3b226b
leptone <leptone55@gmail.com>
2017-03-12 13:21:32 -0700

    build reservation component

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 export default class Reservation extends React.Component {
-    render() (
-        <div>
-            <label>Date</label>
-            <input id='date' name='date' type='text' />
-            <label>Time</label>
-            <input id='time' name='time' type='text' />
-            <label>Name</label>
-            <input id='name' name='name' type='text' />
-        </div>
-    )
+    render()  {
+        return (
+            <div>
+                <label>Date</label>
+                <input id='date' name='date' type='text' />
+                <label>Time</label>
+                <input id='time' name='time' type='text' />
+                <label>Size of Your Party</label>
+                <input id='partySize' name='partySize' type='text' />
+                <label>Name</label>
+                <input id='name' name='name' type='text' />
+            </div>
+        )
+    }
 }
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,16 +2,28 @@ import React from 'react'
 import { IndexLink } from 'react-router'
 import { connect } from 'react-redux'
 
+import Reservation from './Reservation'
 
 export default class Restaurant extends React.Component {
+    state = {
+        showRes: false
+    }
+    handleClick = (e) => {
+        e.preventDefault() 
+        this.setState({ showRes : !this.state.showRes })
+    }
     render() {
         console.log('Restaurant props', this.props)
+        console.log('Reservation', Reservation)
+        let res = ( this.state.showRes ?  <Reservation /> : null ) 
+        console.log('res', res)
         return (
             <div>
                 <IndexLink to='/'>Restaurant</IndexLink>
                 <div> rid: {this.props.routeParams.rid} </div>
                 <div>87%</div>
-                <button>Make Reservation</button>
+                <button onClick={this.handleClick}>Make Reservation</button>
+                {res}
             </div>
         )
     }
