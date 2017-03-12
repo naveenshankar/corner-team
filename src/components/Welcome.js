f67bd05725c140bb6d2f5227c5c26eb968aa15d7
Naveen Shankar <naveenshankarcse@gmail.com>
2017-03-12 14:19:59 -0700

    random changes

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+
+export class Welcome extends React.Component {
+  	render() {
+  			return (
+				<div className="rs_message container">
+				  <h1>Welcome to Corner Team</h1>
+				  <div className="rs_media">
+				  </div>
+			    </div>
+			);
+		}
+}
